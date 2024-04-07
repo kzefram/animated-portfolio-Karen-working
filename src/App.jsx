3c08f98c13909decb09842d18d7c81a8fa220e67
KarenB <karenabourgeois@gmail.com>
2024-04-07 15:00:29 -0300

    Added more files and images

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.scss';
+import Navbar from './Components/navbar/Navbar';
+
+const App = () => {
+  return <div>
+    <section>
+      <Navbar />
+    </section>
+    <section>Parallax</section>
+    <section>About Me</section>
+    <section>Parallax</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section>Portfolio4</section>
+    <section>Portfolio5</section>
+    <section>Portfolio6</section>
+    <section>Contact</section>
+  </div>;
+};
+
+export default App;
